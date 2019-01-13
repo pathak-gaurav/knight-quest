d043ca117758ec2e4098df9fd40ab0b4b4d67dc4
gauravpathak <gauravpathak.spring@gmail.com>
2019-01-13 12:57:13 +0530

    InitializingBean

--- src/main/java/com/gaurav/knightquest/knights/BraveKnight.java
+++ src/main/java/com/gaurav/knightquest/knights/BraveKnight.java
@@ -4,13 +4,15 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BraveKnight implements Knight, BeanNameAware, BeanFactoryAware, ApplicationContextAware, BeanPostProcessor {
+public class BraveKnight implements Knight, BeanNameAware, BeanFactoryAware,
+        ApplicationContextAware, BeanPostProcessor , InitializingBean {
 
     private Quest quest;
 
@@ -50,5 +52,10 @@ public class BraveKnight implements Knight, BeanNameAware, BeanFactoryAware, App
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("\n"+bean.getClass().getName()+" "+beanName);
         return bean;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("After Properties Set");
     }
 }
