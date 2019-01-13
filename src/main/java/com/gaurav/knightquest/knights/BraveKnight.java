e6a201c2307374aee681b81aefca6ba89d353376
gauravpathak <gauravpathak.spring@gmail.com>
2019-01-13 12:34:49 +0530

    Added BeanFactoryAware

--- src/main/java/com/gaurav/knightquest/knights/BraveKnight.java
+++ src/main/java/com/gaurav/knightquest/knights/BraveKnight.java
@@ -1,10 +1,13 @@
 package com.gaurav.knightquest.knights;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BraveKnight implements Knight, BeanNameAware {
+public class BraveKnight implements Knight, BeanNameAware, BeanFactoryAware {
 
     private Quest quest;
 
@@ -20,5 +23,11 @@ public class BraveKnight implements Knight, BeanNameAware {
     @Override
     public void setBeanName(String s) {
         System.out.println("Bean Name: "+s);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        SlayDragonQuest dragonQuest = beanFactory.getBean("slayDragonQuest", SlayDragonQuest.class);
+        dragonQuest.embark();
     }
 }
